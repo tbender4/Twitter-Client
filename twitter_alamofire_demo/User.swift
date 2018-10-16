533164da7916680ede0d2230861b5150f5b4697c
Thomas Bender <tbender4@gmail.com>
2018-10-16 17:40:17 -0400

    added initial files

---
+++ twitter_alamofire_demo/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  twitter_alamofire_demo
+//
+//  Created by Thomas Bender on 10/16/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+  var name: String?
+  
+  init(dictionary: [String : Any]) {
+    super.init()
+  }
+}
